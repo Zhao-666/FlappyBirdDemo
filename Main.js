1aadfd88137087b48f7ce2bc04caf3fd5c51f133
Zhao-666 <2268699370@qq.com>
2018-11-01 22:56:17 +0800

    create ResourceLoader.js

--- Main.js
+++ Main.js
@@ -1,5 +1,14 @@
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
+
 export class Main {
-    constructor(){
-        console.log('HelloWorld!!!')
+    constructor() {
+        this.canvas = document.getElementById('game_canvas')
+        this.ctx = this.canvas.getContext('2d')
+        const loader = ResourceLoader.create()
+        loader.onLoaded(map => this.onResourceFirstLoading(map))
+    }
+
+    onResourceFirstLoading(map) {
+        console.log(map)
     }
 }
